77db7658debfa0cc0917f0d51b69d15a235edb2e
ctello-6connex <ctello@6Connex.com>
2022-03-29 22:52:53 -0500

    Singleton(ZoombiesGenerator,SurvivorCampo), Factory(Caminantes,Rastreros)

---
+++ src/mundo/elementosVivientes/SerViviente.java
@@ -0,0 +1,19 @@
+package mundo.elementosVivientes;
+
+public interface SerViviente {
+	/**
+	 * todo ser viviente en el juego realiza un ataque caracter�stico
+	 * @return
+	 */
+	String ataco();
+	/**
+	 * cambia la salud del ser viviente
+	 * @param nuevaSalud
+	 */
+	void setSalud(byte nuevaSalud);
+	/**
+	 * todo ser viviente contiene salud, representado por un valor num�rico
+	 * @return
+	 */
+	byte getSalud();
+}
---
+++ src/mundo/elementosVivientes/ZoombieGenerator.java
@@ -0,0 +1,27 @@
+package mundo.elementosVivientes;
+
+public class ZoombieGenerator {
+  private static Zombie zombie;
+  static ZoombieGenerator instance;
+  
+  private ZoombieGenerator(){}
+
+  public static ZoombieGenerator getInstance(){
+    if(instance == null){
+      instance = new ZoombieGenerator();
+    }
+    return instance;
+  }
+
+  public Zombie generate(String type, short level, Zombie lejano){
+    if(type.equals("Caminante")){
+      zombie = new Caminante(level, lejano);
+    }else if(type.equals("Rastrero")){
+      zombie = new Rastrero(level, lejano);
+    }
+    return zombie;
+  }
+  static Zombie export(){
+    return zombie;
+  }
+}
